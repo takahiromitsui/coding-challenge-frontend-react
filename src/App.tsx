d26fb05668cb50f9ec20b8c49661c202950a701b
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-03-01 11:29:53 +0100

    Rename the function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Table from './components/Table';
-import { testFetch } from './utils/apiCall';
 // eslint-disable-next-line
 const App: React.FC = () => {
-  const data = testFetch();
-  console.log(data);
   return (
     <>
       <Table />
--- src/utils/apiCall.tsx
+++ src/utils/apiCall.tsx
@@ -1,17 +1,9 @@
-import axios, { AxiosResponse } from 'axios';
 import { google } from 'googleapis';
 
 const spreadsheetId = '1La-EJVOrNt3AwWHYvhuCQ5SRtFE9h_kYjgx0dau1HN4';
 
 // eslint-disable-next-line
-export const tableFetch = async (spreadsheetId: string, sheetId: string) => {
-  // eslint-disable-next-line
-    const url = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/edit#gid=${sheetId}`
-  const res: AxiosResponse = await axios.get(url);
-  return res.data;
-};
-// eslint-disable-next-line
-export const testFetch = async() => {
+export const tableFetch = async() => {
   const auth = new google.auth.GoogleAuth({
     keyFile: 'credential.json',
     scopes: 'https:www.googleapis.com/auth/spreadsheets',
